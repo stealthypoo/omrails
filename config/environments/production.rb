cd19148cb5c83ab39876367594128f6e4f825ea1
Donna Ho <donnaho@gmail.com>
2013-03-29 17:20:59 -0700

    Connect Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,13 @@ Omrails::Application.configure do
 
   #In production, :host should be set to the actual host of your application.
 
+  # Configuring Amazon S3 for Paperclip file uploads
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+    }
 end
